e7e4683b8107909756a8baab88c22715533e72b3
artemmisesin@gmail.com <artemmisesin@artems-macbook-pro.local>
2017-02-21 18:30:46 +0200

    Initial Commit

---
+++ DBProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DBProject
+//
+//  Created by Artem Misesin on 2/21/17.
+//  Copyright © 2017 Artem Misesin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DBProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DBProject
+//
+//  Created by Artem Misesin on 2/21/17.
+//  Copyright © 2017 Artem Misesin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
